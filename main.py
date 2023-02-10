50c0d98bce42d609801ce4b9e4713541674fad2f
Andino Boerst <andino.boerst@gmail.com>
2023-02-10 13:45:49 +0100

    add dataset

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import myPackages.neural_network as nn
-
-
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
 
 
 def main():
     print("Start neural netwrok.")
+
+    digits = datasets.load_digits()
+    # plot_digits(digits)
+
+    # flatten the images
+    n_samples = len(digits.images)
+    data = digits.images.reshape((n_samples, -1))
+
+    # Split data into 50% train and 50% test subsets
+    X_train, X_test, y_train, y_test = train_test_split(
+        data, digits.target, test_size=0.5, shuffle=False
+    )
+
     my_NN = nn.NeuralNetwork()
     my_NN.add_layer(3, layer_type="input")
     my_NN.add_layer(5)
@@ -16,6 +29,14 @@ def main():
     #print([node.bias for node in my_NN.layers[0].nodes])
     print(my_NN.predict([1, 2, 0.3]))
 
+
+def plot_digits(digits):
+    _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
+    for ax, image, label in zip(axes, digits.images, digits.target):
+        ax.set_axis_off()
+        ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
+        ax.set_title("Training: %i" % label)
+    plt.show()
 
 if __name__=="__main__":
     main()
--- myPackages/back_propagation.py
+++ myPackages/back_propagation.py
@@ -10,8 +10,15 @@ def mini_prop(self, X: np.array([[]]), y: np.array([])) -> None:
     '''
     Backpropagation after a mini-batch
     '''
+    # Divide the sets and call batch_prop() on them
 
 def batch_prop(self, X: np.array([[]]), y: np.array([])) -> None:
     '''
     Backpropagation for the entire batch
+    '''
+    # call sample_prop() on each sample and average the results
+
+def apply_prop(self):
+    '''
+    Apply the defined weights and biases to the neural network
     '''
