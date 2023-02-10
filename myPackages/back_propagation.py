c7bf4cfc778f0e44793f0d068db891d68b3847d6
Andino Boerst <andino.boerst@gmail.com>
2023-02-10 20:00:26 +0100

    commit

--- myPackages/back_propagation.py
+++ myPackages/back_propagation.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def sample_prop(self, x: np.array([]), y: np.array([]), update: bool=True) -> np.array([[]]):
+def sample_prop(self, x: np.array([]), y: np.array([]), update: bool=True) -> list:
     '''
     Backpropagation after every sample
     '''
@@ -31,7 +31,7 @@ def mini_prop(self, X: np.array([[]]), Y: np.array([[]]), num_batches: int=5) ->
 
 
 
-def batch_prop(self, X: np.array([[]]), Y: np.array([[]]), update: bool=True) -> np.array([[]]):
+def batch_prop(self, X: np.array([[]]), Y: np.array([[]]), update: bool=True) -> list:
     '''
     Backpropagation for the entire batch
     '''
@@ -39,7 +39,14 @@ def batch_prop(self, X: np.array([[]]), Y: np.array([[]]), update: bool=True) ->
     updated_weights = []
     for x, y in zip(X,Y):
         updated_weights.append(self.sample_prop(x,y,update=False)) # This will be 3 dimensional!!
-    final_updated_weights = np.mean(updated_weights, axis=0) # Not sure this will work
+    #final_updated_weights = np.mean(updated_weights, axis=0) # Not sure this will work
+    final_updated_weights = updated_weights[0]
+    for entry in updated_weights[1:]:
+        for index, l in enumerate(entry):
+            final_updated_weights[index] = [sum(i) for i in zip(final_updated_weights[index],l)]
+    for i, entry in enumerate(final_updated_weights):
+        for j, item in enumerate(entry):
+            final_updated_weights[i][j] = item / len(updated_weights)
     if update:
         self.apply_prop(final_updated_weights)
     return final_updated_weights
